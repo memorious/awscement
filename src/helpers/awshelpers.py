f267396b83f90b28f4821385d874063bf05553f6
Alexander Hall <alexander.hall@Alexanders-MBP.home>
2017-04-12 00:16:08 -0400

    Here We Go :)

---
+++ src/helpers/awshelpers.py
@@ -0,0 +1,19 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+import time
+
+__author__ = "alexander.hall"
+__date__ = "$Apr 11, 2017 10:11:11 AM$"
+
+if __name__ == "__main__":
+    print('loading awshelpers')
+else:
+    print('loading awshelpers')
+    
+
+timeval = time.strftime("%c")
+
+def toLine(valueToPrint):
+    return(timeval + " : " + valueToPrint)
