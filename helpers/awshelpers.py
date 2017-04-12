2a7c6b5079895a742be15f7709c0e89373847434
Alexander Hall <alexander.hall@Alexanders-MBP.home>
2017-04-12 01:10:41 -0400

    updating helper and cleaning up s3interface a bit

--- helpers/awshelpers.py
+++ helpers/awshelpers.py
@@ -1,19 +1,39 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
+# MIT License
+#
+# Copyright (c) 2017 memorious
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 import time
-
-__author__ = "alexander.hall"
-__date__ = "$Apr 11, 2017 10:11:11 AM$"
-
-if __name__ == "__main__":
-    print('loading awshelpers')
-else:
-    print('loading awshelpers')
-    
 
 timeval = time.strftime("%c")
 
 def toLine(valueToPrint):
     return(timeval + " : " + valueToPrint)
+
+def s3Connect(key_id,access_key):
+    #let's load up boto3
+    import boto3
+    #let's start a connection
+    s3 = boto3.resource(
+        's3',
+        aws_access_key_id = key_id,
+        aws_secret_access_key = access_key)   #lets connect using credentials and region stored in ~/.aws/credentials and ~/.aws/config
+
+    return s3
