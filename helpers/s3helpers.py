aa1c9e829a657d2837e1cf21699ffbd8a5901f6d
Alexander Hall <alexander.hall@Alexanders-MBP.home>
2017-04-13 00:31:10 -0400

    Adding count function and renaming main helper to s3 helper

--- helpers/s3helpers.py
+++ helpers/s3helpers.py
@@ -27,13 +27,48 @@ timeval = time.strftime("%c")
 def toLine(valueToPrint):
     return(timeval + " : " + valueToPrint)
 
-def s3Connect(key_id,access_key):
+def s3Connect(self):
+    if self.app.pargs.key_id is None:
+        print(toLine('Enter AWS Access Key ID'))
+        return None
+    if self.app.pargs.access_key is None:
+        print(toLine('Enter AWS Secret Access Key'))
+        return
     #let's load up boto3
     import boto3
     #let's start a connection
     s3 = boto3.resource(
         's3',
-        aws_access_key_id = key_id,
-        aws_secret_access_key = access_key)   #lets connect using credentials and region stored in ~/.aws/credentials and ~/.aws/config
+        aws_access_key_id = self.app.pargs.key_id,
+        aws_secret_access_key = self.app.pargs.access_key)   #lets connect using credentials and region stored in ~/.aws/credentials and ~/.aws/config
 
     return s3
+
+def s3List(self, s3):
+    if self.app.pargs.bucket is None:
+        print(toLine('Enter target Bucket'))
+        return None
+    if self.app.pargs.dir is None:
+        print(toLine('No target directory entered'))
+
+    #let's find our bucket
+    if s3.Bucket(self.app.pargs.bucket) in s3.buckets.all():
+        bucket = s3.Bucket(self.app.pargs.bucket)
+    else:
+        print(toLine('Target Bucket Not Found'))
+        return None
+
+    fileList = []
+    if self.app.pargs.dir is None:
+        for obj in bucket.objects.all():
+            filepath = obj.key.split('/')
+            filename = filepath[-1]
+            fileList.append(filename)
+    else:
+        for obj in bucket.objects.all():
+            if self.app.pargs.dir in obj.key in obj.key:
+                filepath = obj.key.split('/')
+                filename = filepath[-1]
+                fileList.append(filename)
+
+    return fileList
